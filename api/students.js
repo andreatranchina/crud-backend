ab979d928b01eba28f9c4bd3c5917c7a82bad3e2
Andrea <Andrea.Tranchina82@gmail.com>
2023-06-30 14:51:51 -0400

    prepared get all students route

--- api/students.js
+++ api/students.js
@@ -5,5 +5,17 @@ const { Student } = require('../db/models');
 
 // root: http://localhost/8080/api/students
 
+router.get('/', async (req, res, next) => {
+    try{
+        const allStudents = await Student.findAll();
+        allStudents? res.status(200).json(allShoes): res.status(404).send('Student Listing Not Found');
+
+    }
+    catch(error){
+        // console.log(error.message);
+        next(error);
+    }
+})
+
 
 module.exports = router;
