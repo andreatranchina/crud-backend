38258e557a76596e913cf50c9a6f27f246c677ad
Andrea <Andrea.Tranchina82@gmail.com>
2023-06-30 21:29:41 -0400

    fixed typo in students route

--- api/students.js
+++ api/students.js
@@ -8,7 +8,7 @@ const { Student } = require('../db/models');
 router.get('/', async (req, res, next) => {
     try{
         const allStudents = await Student.findAll();
-        allStudents? res.status(200).json(allShoes): res.status(404).send('Student Listing Not Found');
+        allStudents? res.status(200).json(allStudents): res.status(404).send('Student Listing Not Found');
 
     }
     catch(error){
