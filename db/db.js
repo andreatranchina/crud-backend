3c9d9a12e5f7fb6024003ac87d93a96d90d3f315
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-07 17:30:46 -0400

    create dev branch for local

--- db/db.js
+++ db/db.js
@@ -3,11 +3,11 @@ const {name} = require('../package.json');
 const pg = require('pg');
 //name === crud-backend
 
-const db = new Sequelize(process.env.POSTGRES_URL + "?sslmode=require", {
-    logging: false,
-});
-// const db = new Sequelize(`postgres://localhost:5432/${name}`, {
+// const db = new Sequelize(process.env.POSTGRES_URL + "?sslmode=require", {
 //     logging: false,
 // });
+const db = new Sequelize(`postgres://localhost:5432/${name}`, {
+    logging: false,
+});
 
 module.exports = db;
--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const pg = require('pg');
 
 //Middleware and mounting on API
 // app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors({origin: 'https://64a87a606a675c0f1ec4cb45--sparkly-daifuku-f0cc8c.netlify.app'}));
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
+// app.use(cors({origin: 'https://64a87a606a675c0f1ec4cb45--sparkly-daifuku-f0cc8c.netlify.app'}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json());
 app.use('/api', require('./api'));
 
