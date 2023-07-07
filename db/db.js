4b09c4dbfd18dc8e8e6ed830f5b133662b319e50
Andrea <Andrea.Tranchina82@gmail.com>
2023-07-07 04:38:41 -0400

    attempt vercel deployment

--- db/db.js
+++ db/db.js
@@ -3,11 +3,7 @@ const {name} = require('../package.json');
 const pg = require('pg');
 //name === crud-backend
 
-const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-  })
-
-const db = new Sequelize(`postgres://localhost:5432/${name}`, {
+const db = new Sequelize(process.env.POSTGRES_URL + "?sslmode=require", {
     logging: false,
 });
 
