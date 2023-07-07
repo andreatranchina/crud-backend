54991ff7adb5dd44f0b564da700bf01ae253def6
Andrea <Andrea.Tranchina82@gmail.com>
2023-07-07 04:50:48 -0400

    attempt vercel deployment

--- db/db.js
+++ db/db.js
@@ -2,6 +2,7 @@ const { Sequelize } = require('sequelize');
 const {name} = require('../package.json');
 const pg = require('pg');
 //name === crud-backend
+require('dotenv').config();
 
 const db = new Sequelize(process.env.POSTGRES_URL + "?sslmode=require", {
     logging: false,
